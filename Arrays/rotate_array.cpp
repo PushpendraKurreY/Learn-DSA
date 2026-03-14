8e68f04490cc00a5b7c12746350fc39848ee29fa
pushpendra kurrey <pushpendrakurreybemetara@gmail.com>
2026-03-14 18:05:20 +0000

    Added rotate_array file.

---
+++ Arrays/rotate_array.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> rotateArray(vector<int> &arr, int N) {
+    int n=arr.size();
+    N=N%arr.size();
+    vector<int> newArr(n);
+    for(int i=0; i<n-N; i++) {
+      newArr[i+N]=arr[i];
+    }
+    for(int i=0; i<N; i++) {
+      newArr[i]=arr[n-N+i];
+    }
+    return newArr;
+}
+
+int main() {
+    vector<int> arr={0,1,2,3,4,5,6,7,8,9};
+    int N;
+    cin >> N;
+    arr=rotateArray(arr, N);
+    for(int i=0; i<arr.size(); i++) {
+      cout << arr[i] << " ";
+    }
+    return 0;
+}
