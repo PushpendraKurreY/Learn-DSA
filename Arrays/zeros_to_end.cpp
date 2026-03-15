b2841ce26d38b594a9739e15f2a34bf919c2c795
pushpendra kurrey <pushpendrakurreybemetara@gmail.com>
2026-03-15 18:21:08 +0000

    Added arrays/zeros_to_end file.

---
+++ Arrays/zeros_to_end.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> zerosToEnd(vector<int> &arr) {
+    vector<int> newArr;
+    int count=0;
+    for(int i=0; i<arr.size(); i++) {
+        if(arr[i]!=0) {
+            newArr.push_back(arr[i]);
+            count++;
+        }
+    }
+    for(int i=count; i<arr.size(); i++) {
+        newArr.push_back(0);
+    }
+  return newArr;
+}
+
+int main() {
+    vector<int> arr={1,0,2,0,3,0,0,0,0,0};
+    arr=zerosToEnd(arr);
+    for(int i=0; i<arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
