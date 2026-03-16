f320889b29681d9826e55e2183e5dc6e1e6b6ec2
pushpendra kurrey <pushpendrakurreybemetara@gmail.com>
2026-03-16 17:02:24 +0000

    Added min_increments_to_make_equal file.

---
+++ Arrays/min_increments_to_make_equal.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int minIncrements(vector<int> &arr, int k) {
+    int count=0;
+    int max= *max_element(arr.begin(), arr.end());
+    for(auto x:arr) {
+      if(((max-x)%k)!=0) return -1;
+      else count+=abs(max-x);
+    }
+    return count/k;
+}
+
+int main() {
+    int k=3;
+    vector<int> arr={0,0,0,0};
+    cout << minIncrements(arr, k);
+    return 0;
+}
