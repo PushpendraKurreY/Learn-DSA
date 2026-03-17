062d498a4e40e25f4a349c285e75e6351320e3f2
pushpendra kurrey <pushpendrakurreybemetara@gmail.com>
2026-03-17 18:46:19 +0000

    Added sum_of_all_subarrays file.

---
+++ Arrays/sum_of_all_subarrays.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int subarraySum(vector<int> &arr) {
+    int n=arr.size(), sum=0;
+    for(int i=0; i<n; i++) {
+        sum+=arr[i]*(i+1)*(n-i);
+    }
+return sum;
+}
+
+int main() {
+    vector<int> arr={3,2,6,4,1};
+    cout << subarraySum(arr);
+    return 0;
+}
